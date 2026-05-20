d9e6013908af240996ca209a86b90b35d701e72c
pradipraj142-rgb <pradipraj142@gmail.com>
2026-05-20 23:49:29 +0545

    Add App.js - Main application component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import DreamHomeNepal from './components/DreamHomeNepal';
+import './index.css';
+
+function App() {
+  return <DreamHomeNepal />;
+}
+
+export default App;
